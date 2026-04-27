31d390274bc8a6ec1e269a533fe02a236a15e9b0
Dhruvil <dhruvilgandhi1203@gmail.com>
2026-04-27 13:55:37 +0530

    streamlit issues

--- app/database.py
+++ app/database.py
@@ -2,22 +2,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-from pathlib import Path
 
-env_path = Path(__file__).resolve().parent.parent / ".env"
-load_dotenv(env_path)
+load_dotenv() 
 
-DATABASE_URL = os.getenv("DATABASE_URL")# The engine is the core interface to the database
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not found! Check your .env or Cloud Secrets.")
+
 engine = create_engine(
     DATABASE_URL,
-    connect_args={
-        "connect_timeout": 10,  
-    },
     pool_pre_ping=True,
     pool_recycle=300
 )
-# The session is what we use to query and add data
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# All our models will inherit from this Base class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
