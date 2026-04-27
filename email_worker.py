d0da75d83c923babd52f2811b45e870b65dd85f1
Dhruvil <dhruvilgandhi1203@gmail.com>
2026-04-28 00:40:42 +0530

    we don't need schedule module anymore

--- email_worker.py
+++ email_worker.py
@@ -1,7 +1,6 @@
 import os
 import smtplib
 import time
-import schedule
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime, timedelta, timezone
